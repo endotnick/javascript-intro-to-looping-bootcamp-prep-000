08a637541fbc59e29ae98ee5c26ef38d2cbc6dd8
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-12 22:30:59 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+  }
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+function doWhileLooop(array) {
+  do {
+    array.pop();
+  } while(array.length > 1);
+}
