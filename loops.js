c67559c17c3b9e6ee16a5291e69a0f6347773701
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-12 22:35:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,6 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push(`I am ${i} strange loop.`);
-    } else {
-      array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
-    }
+  for (let i = 0; i < 25; i++) {    
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
   }
   return array;
 }
